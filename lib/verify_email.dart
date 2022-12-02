1a05636308927d29291a7e25f080da9b5879a375
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2022-12-02 12:08:10 -0300

    change page verify email

--- lib/verify_email.dart
+++ lib/verify_email.dart
@@ -1,5 +1,8 @@
+import 'dart:developer';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:mynotes/core/routes/routes.dart';
 
 class VerifyEmail extends StatefulWidget {
   const VerifyEmail({Key? key}) : super(key: key);
@@ -12,31 +15,85 @@ class _VerifyEmailState extends State<VerifyEmail> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.black54),
+        shadowColor: Colors.transparent,
+        backgroundColor: Colors.transparent,
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
+            const SizedBox.shrink(),
             const Text(
-              'Por favor vefique seu email',
+              'Enviamos um email de verificação para você,\n por favor verifique sua conta.',
               style: TextStyle(
                 color: Colors.black45,
               ),
+              textAlign: TextAlign.center,
             ),
-            const SizedBox(
-              height: 16,
-            ),
-            TextButton(
-                onPressed: () async {
-                  final user = FirebaseAuth.instance.currentUser;
-                  await user?.sendEmailVerification();
-                },
-                child: const Text(
-                  "Verificar",
-                  style: TextStyle(
-                    color: Colors.black45,
-                    decoration: TextDecoration.underline,
-                  ),
-                )),
+            Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Caso não tenha recebido email:',
+                      style: TextStyle(
+                        color: Colors.black45,
+                      ),
+                    ),
+                    TextButton(
+                        style: ButtonStyle(
+                          tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                          minimumSize: MaterialStateProperty.all(Size.zero),
+                          padding: MaterialStateProperty.all(
+                              const EdgeInsets.only(left: 2)),
+                        ),
+                        onPressed: () async {
+                          final user = FirebaseAuth.instance.currentUser;
+                          await user?.sendEmailVerification();
+                        },
+                        child: const Text(
+                          "Clique aqui",
+                          style: TextStyle(
+                            color: Colors.black45,
+                            decoration: TextDecoration.underline,
+                          ),
+                        )),
+                  ],
+                ),
+                TextButton(
+                    style: ButtonStyle(
+                      tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                      minimumSize: MaterialStateProperty.all(Size.zero),
+                      padding: MaterialStateProperty.all(
+                          const EdgeInsets.only(top: 16)),
+                    ),
+                    onPressed: () async {
+                      await FirebaseAuth.instance.signOut();
+
+                      if (!mounted) {
+                        log('erro no mounted');
+                        return;
+                      }
+                      Navigator.of(context).pushNamedAndRemoveUntil(
+                        loginRoute,
+                        (_) => false,
+                      );
+                    },
+                    child: const Text(
+                      "Sair da conta",
+                      style: TextStyle(
+                        color: Colors.black45,
+                        decoration: TextDecoration.underline,
+                      ),
+                    )),
+                const SizedBox(
+                  height: 16,
+                ),
+              ],
+            )
           ],
         ),
       ),
